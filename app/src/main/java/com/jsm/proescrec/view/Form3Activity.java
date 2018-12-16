611be8bcf8e74c78cafcd0322510ea8e5bdd8c08
Jonthan Sanchez <jonathan.sanchezmunoz@gm.com>
2018-12-16 12:20:39 -0500

    ultimos cambios

---
+++ app/src/main/java/com/jsm/proescrec/view/Form3Activity.java
@@ -0,0 +1,14 @@
+package com.jsm.proescrec.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form3);
+    }
+}
